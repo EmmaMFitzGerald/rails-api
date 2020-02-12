d460d5bd7b75f57c8d5ff2d870ea60c6655dce3e
Emma FitzGerald <emmamariafitzgerald@gmail.com>
2020-02-12 14:40:23 -0500

    added controller actions

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -5,4 +5,40 @@ class Api::V1::NotesController < ApplicationController
 
         render json: @notes, status: 200 
     end 
+
+    def show
+        @note = Note.find(params[:id])
+
+        render json: @note, status: 200
+    end 
+
+    def create
+        @note = Note.new
+
+        if @note.save
+            render json: @note
+        else 
+            render json: @note.errors
+        end 
+    end 
+
+    def update
+        @note = Note.find(params[:id])
+
+        if @note.update(note_params)
+            render json: @note
+        else
+            render json: @note.errors, status: :unprocessable_entity
+        end
+    end 
+
+    def destroy
+        @note.destroy
+    end
+
+    private 
+
+    def note_params
+        params.require(:note).permit(:body)
+    end 
 end
